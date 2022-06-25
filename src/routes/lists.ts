bdd564a60c143ebdcd5be01443c2aafe656073d0
StefanBehring <chadhz@gmx.de>
2022-06-25 20:40:49 +0200

    Add new routes to lists

--- src/routes/lists.ts
+++ src/routes/lists.ts
@@ -2,7 +2,10 @@ import { Router } from 'express'
 
 import {
   getCurrentDividendsRate,
+  getConstantDividendRises,
   getLast10YearsDividendsRate,
+  getCurrentDividendsRateAktie,
+  getLast10YearsDividendsRateAktie,
 } from '../controllers/lists'
 
 const router = Router()
@@ -13,7 +16,19 @@ const router = Router()
 
 router.get('/current-dividends-rate', getCurrentDividendsRate)
 
+router.get(
+  '/current-dividends-rate-aktie/:aktieId',
+  getCurrentDividendsRateAktie
+)
+
+router.get('/constant-dividenden-rises', getConstantDividendRises)
+
 router.get('/last-10-years-dividends-rate', getLast10YearsDividendsRate)
+
+router.get(
+  '/last-10-years-dividends-rate-aktie/:aktieId',
+  getLast10YearsDividendsRateAktie
+)
 
 // router.patch('/:userId', updateUser)
 
